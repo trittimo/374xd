cae04b4f46bdd26f129acef1721e8e7cbc5f056b
Michael Trittin <michaeltrittin@gmail.com>
2017-01-26 18:55:48 -0500

    Just a few misc changes

--- UMLGenerator/src/generator/ILink.java
+++ UMLGenerator/src/generator/ILink.java
@@ -3,4 +3,8 @@ package generator;
 public interface ILink {
 	public String getRelationship();
 	public String getAttributes();
+	
+	// Overridden by object, but should be implemented by all implementing classes
+	public String toString();
+	public boolean equals(Object o);
 }
