f6075bc82bad9f8be0957cd5210afa1bc8e7bf79
Michael Trittin <trittimo@rose-hulman.edu>
2017-01-26 23:21:37 -0500

    Added inheritance analyzer

---
+++ UMLGenerator/src/generator/analyzers/violation/InheritanceAnalyzer.java
@@ -0,0 +1,43 @@
+package generator.analyzers.violation;
+
+import java.io.IOException;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.ClassNode;
+
+import generator.Graph;
+import generator.INode;
+import generator.analyzers.IAnalyzer;
+import generator.commands.CMDParams;
+import generator.factories.IGraphFactory;
+
+public class InheritanceAnalyzer implements IAnalyzer {
+
+	@Override
+	public boolean analyze(Graph graph, CMDParams params, IGraphFactory factory) {
+		ClassReader reader = null;
+		for (String name : graph.getNodes().keySet()) {
+			INode node = graph.getNodes().get(name);
+			try {
+				reader = new ClassReader(node.getQualifiedName());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			ClassNode classNode = new ClassNode();
+			reader.accept(classNode, ClassReader.EXPAND_FRAMES);
+			
+			// Ensure it is a regular class (i.e. not abstract or an interface or an annotation)
+			if ((classNode.access & (Opcodes.ACC_INTERFACE | Opcodes.ACC_ANNOTATION | Opcodes.ACC_ABSTRACT)) > 0) {
+				continue;
+			}
+			
+			// If there is a superclass that is not Object
+			if (classNode.superName != null && !classNode.superName.equals("java/lang/Object")) {
+				System.out.printf("Change color cuz bad: %s\n", classNode.name);
+			}
+		}
+		return false;
+	}
+
+}
